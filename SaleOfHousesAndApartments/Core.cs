fd5d12ba7b23c655041a526dbfcca0ab3b403c23
KKozhinov <82210482+Illarion03@users.noreply.github.com>
2021-05-31 08:35:14 +0300

    first commit

---
+++ SaleOfHousesAndApartments/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleOfHousesAndApartments
+{
+    class Core
+    {
+        public static iartushinEntities DB = new iartushinEntities();
+    }
+}
